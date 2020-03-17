54ed482c16c4026194f2cdf2ac44d1042a083255
mathiastj <2278040+mathiastj@users.noreply.github.com>
2020-03-17 18:19:39 +0100

    Updates

---
+++ precache-manifest.24bf75ca721eee12d3786f041e94d800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fb48149a045b833e710c8ab8886801",
+    "url": "/corona-graphs/index.html"
+  },
+  {
+    "revision": "6011cedea8cacd7b040f",
+    "url": "/corona-graphs/static/css/main.94717d5f.chunk.css"
+  },
+  {
+    "revision": "3722c6340a81a0e77c33",
+    "url": "/corona-graphs/static/js/2.07fad896.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/corona-graphs/static/js/2.07fad896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6011cedea8cacd7b040f",
+    "url": "/corona-graphs/static/js/main.20653a57.chunk.js"
+  },
+  {
+    "revision": "3eb17b661d7876236278",
+    "url": "/corona-graphs/static/js/runtime-main.57dc8888.js"
+  }
+]);
