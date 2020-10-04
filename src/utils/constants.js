003fddd1b799cd4f61bcfe4dfb274a6f77b2f4bc
mathiastj <2278040+mathiastj@users.noreply.github.com>
2020-10-04 14:19:22 +0200

    Use constants instead of repeatedly used strings

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const DATA_TYPES = {
+  NEW_CASES: 'newCases',
+  NEW_DEATHS: 'newDeaths',
+  TOTAL_DEATHS: 'totalDeaths',
+  TOTAL_CASES: 'totalCases',
+}
+
+export const DATA_MODIFIERS = {
+  ROLLING: 'Rolling',
+  PER_CAPITA: 'PerCapita',
+}
